c6609228e13a134ceafde76a48ec1681d5926451
Technic017 <91904374+Technic017@users.noreply.github.com>
2023-11-27 15:25:24 +0100

    Create Sync.py

---
+++ Sync.py
@@ -0,0 +1,12 @@
+#########################################
+#   Name: Sync                          #
+#   Version: 0.0.1 (alpha)              #
+#   Author: Technic017                  #
+#   Discription:                        #
+#       Program do synchronizacji       #
+#       dokumentow dysk <> PC           #
+#   Date: 13/11/2023                    #
+#########################################
+
+import os
+
