f62de74ab8fafb32816393df9f9dedcb260f6515
Technic017 <91904374+Technic017@users.noreply.github.com>
2023-11-28 15:54:01 +0100

    Update Sync.py
    Zmiana na obiektowy jezyk

--- Sync.py
+++ Sync.py
@@ -10,27 +10,29 @@
 
 import os
 
-# Stale
-FOLDERY = ("./TEST_PC", "./TEST_DYSK") # 0 - folder na PC, 1 - folder na dysku
+class FolderComparer:
 
-def zawartoscFolderow():
-    pass
+    def __init__(self, folder0, folder1):
+        self.folder0 = folder0
+        self.folder1 = folder1
 
-def porownywanieFolderow(folder0, folder1):
+    def get_files_to_sync(self):
+        files_to_sync = []
 
-    folder_dysk = os.listdir(folder1)
-    folder_pc = os.listdir(folder0)
+        for file_d in os.listdir(self.folder1):
+            if file_d not in os.listdir(self.folder0):
+                files_to_sync.append((0, file_d))
 
-    pliki_to_sync = []
+        for file_p in os.listdir(self.folder0):
+            if file_p not in os.listdir(self.folder1):
+                files_to_sync.append((1, file_p))
+        
+            if file_p in os.listdir(self.folder1):
+                files_to_sync.append((2, file_p))
 
-    for file_d in folder_dysk:
-        if file_d not in folder_pc:
-            pliki_to_sync.append([0,file_d])
+        return files_to_sync
 
-    for file_p in folder_pc:
-        if file_p not in folder_dysk:
-            pliki_to_sync.append([1,file_p])
-    
-    return pliki_to_sync
-
-print(porownywanieFolderow(FOLDERY[0], FOLDERY[1]))
+if __name__ == "__main__":
+    comparer = FolderComparer("./TEST_PC", "./TEST_DYSK")
+    files_to_sync = comparer.get_files_to_sync()
+    print(files_to_sync)
